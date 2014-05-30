7ba0ea5219a0689b785fbf8cef41076cced0da92
RichardCoded <talentaintenough@gmail.com>
2014-05-30 23:09:01 +0200

    Change-Id: Ibf912391d94b1db54f547a15cb852ca895f5f699

---
+++ ChatClient/src/Auswertung/StaticServerAnswerResources.java
@@ -0,0 +1,49 @@
+package Auswertung;
+
+import javax.swing.JOptionPane;
+
+import Model.Message;
+
+public class StaticServerAnswerResources 
+{
+	public static final String Erfolgreich = "erfolgreich";
+	public static final String Fehlgeschlagen = "fehlgeschlagen";
+	public static final String Error = "Error";
+	
+	public static String getChatOutputEvaluated(Message message)
+	{
+		return getSenderReceiverEvaluated(message) + " : " + message.getContent();
+	}
+	
+	private static String getSenderReceiverEvaluated(Message message)
+	{
+		return "[" + getSender(message) + " -> " + getReceiver(message) + "]";
+	}
+	
+	private static String getSender(Message message)
+	{
+		if(!message.getSender().isEmpty())
+		{
+			return message.getSender();
+		}
+		else
+		{
+			return "Server";
+		}
+	}
+	
+	private static String getReceiver(Message message)
+	{
+		if(!message.getReceiver().isEmpty())
+		{
+			return "Me";
+		}
+		else
+		{
+			return message.getRoom();
+			//andere Option:
+			//receiver = "All";
+		}
+		
+	}		
+}
